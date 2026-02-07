6accf288181083e49b985c1364381ec3abb27c67
ricky <19902266668@163.com>
2026-02-08 00:29:46 +0800

    feat: repository数据访问模块新增对Kafka发送消息的相关方法, http-feed和ws-listener模块均需要引入data-repository(已引入)

---
+++ swaybridge-demo/src/main/java/com/swaybridge/demo/controller/ExecutionController.java
@@ -0,0 +1,25 @@
+package com.swaybridge.demo.controller;
+
+import com.swaybridge.common.model.dto.response.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("execution")
+public class ExecutionController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @GetMapping("todo1")
+    public Result<String> todo1() {
+        kafkaTemplate.send("todo1.topic", "todo1");
+        return Result.ok("推送Kafka成功");
+    }
+
+}
