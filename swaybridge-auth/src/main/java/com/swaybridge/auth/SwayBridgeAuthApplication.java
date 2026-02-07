82c1a7f59ffbfe94fe880a14f63fd1f1a11b8974
ricky <19902266668@163.com>
2026-02-07 16:05:51 +0800

    feat: 添加auth登录认证模块开发, redis全局存储token

---
+++ swaybridge-auth/src/main/java/com/swaybridge/auth/SwayBridgeAuthApplication.java
@@ -0,0 +1,11 @@
+package com.swaybridge.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwayBridgeAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SwayBridgeAuthApplication.class, args);
+    }
+}
